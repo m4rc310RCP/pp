17a7abed0708014ba36565d410ae91bf3fe791bc
m4rc310RCP <marcelo.utfpr@me.com>
2023-06-07 13:09:04 -0300

    Deploy to dokku in ~>

--- src/main/java/foundation/cmo/api/services/graphql/MServiceInit.java
+++ src/main/java/foundation/cmo/api/services/graphql/MServiceInit.java
@@ -1,7 +1,11 @@
 package foundation.cmo.api.services.graphql;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import foundation.cmo.api.db.models.location.MCity;
 import foundation.cmo.api.db.models.location.MDistrict;
@@ -13,27 +17,65 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @Service
 @GraphQLApi
 public class MServiceInit {
-	
-	@Autowired private MDistrictRepository districtRepository;
+
+	@Autowired
+	private MDistrictRepository districtRepository;
+	private long i ;
 
 	@GraphQLQuery(name = "test_aplication")
 	public String testApplication() {
-		
-		MDistrict dis = new MDistrict();
-		dis.setCity(new MCity());
-		dis.getCity().setState(new MState());
-		
-		dis.setId(123L);
-		dis.setName("Test District");
-		
-		dis.getCity().setId(12L);
-		dis.getCity().setName("Test City");
-		
-		dis.getCity().getState().setId(1L);
-		dis.getCity().getState().setName("Test State");;
-		
-		districtRepository.save(dis);
-		
+
+		String surl = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos";
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity(surl, String.class);
+		if (response.getStatusCode().is2xxSuccessful()) {
+			// A requisição foi bem-sucedida
+			String responseBody = response.getBody();
+
+			JSONArray array = new JSONArray(responseBody);
+			array.forEach(obj -> {
+				JSONObject dist = (JSONObject) obj;
+//            	System.out.println(dist.get("nome"));
+				MDistrict dis = new MDistrict();
+				dis.setId(dist.getLong("id"));
+				dis.setName(dist.getString("nome"));
+				
+				MCity city = new MCity();
+				
+				city.setId(dist.getJSONObject("municipio").getLong("id"));
+				city.setName(dist.getJSONObject("municipio").getString("nome"));
+				
+				dis.setCity(city);
+				
+				districtRepository.save(dis);
+				
+				if ((i++) % 200 == 0) {
+					districtRepository.flush();
+				}
+				
+				
+//				dis.setCity(new MCity());
+//				dis.getCity().setState(new MState());
+			});
+
+		} else {
+			// A requisição falhou
+			System.out.println("Falha na requisição: " + response.getStatusCode());
+		}
+
+//		
+//		dis.setId(123L);
+//		dis.setName("Test District");
+//		
+//		dis.getCity().setId(12L);
+//		dis.getCity().setName("Test City");
+//		
+//		dis.getCity().getState().setId(1L);
+//		dis.getCity().getState().setName("Test State");;
+//		
+//		districtRepository.save(dis);
+
 		return "OK";
 	}
 }
