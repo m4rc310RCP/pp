e27e08ac283ca7fa8bfd9948fc53f733d8839956
m4rc310RCP <marcelo.utfpr@me.com>
2023-06-08 12:57:10 -0300

    Deploy to dokku in ~>

--- src/main/java/foundation/cmo/api/db/repositories/MDistrictRepository.java
+++ src/main/java/foundation/cmo/api/db/repositories/MDistrictRepository.java
@@ -1,8 +1,12 @@
 package foundation.cmo.api.db.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import foundation.cmo.api.db.models.location.MCity;
 import foundation.cmo.api.db.models.location.MDistrict;
 
 public interface MDistrictRepository extends JpaRepository<MDistrict, Long> {
+	List<MDistrict> findAllByCity(MCity city);
 }
--- src/main/java/foundation/cmo/api/services/graphql/MServiceLocation.java
+++ src/main/java/foundation/cmo/api/services/graphql/MServiceLocation.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import foundation.cmo.api.db.models.location.MCity;
+import foundation.cmo.api.db.models.location.MDistrict;
 import foundation.cmo.api.db.repositories.MCityRepository;
+import foundation.cmo.api.db.repositories.MDistrictRepository;
 import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLContext;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
@@ -15,8 +18,9 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @GraphQLApi
 public class MServiceLocation {
 
-//	@Autowired
-//	private MDistrictRepository districtRepository;
+	@Autowired
+	private MDistrictRepository districtRepository;
+	
 	@Autowired
 	private MCityRepository cityRepository;
 	
@@ -25,4 +29,16 @@ public class MServiceLocation {
 	public List<MCity> getState(@GraphQLArgument(name = "cd_uf") String acronym) {
 		return cityRepository.findAllByStateAcronym(acronym);
 	}
+	
+	@GraphQLQuery(name = "obter_municipio")
+	public MCity getCity(@GraphQLArgument(name = "cd_ibge") Long id) {
+		return cityRepository.findById(id).orElse(null);
+	}
+	
+	
+	@GraphQLQuery(name = "ls_distritos")
+	public List<MDistrict> getDistrictsFromCity(@GraphQLContext MCity city){
+		return districtRepository.findAllByCity(city);
+	}
+	
 }
